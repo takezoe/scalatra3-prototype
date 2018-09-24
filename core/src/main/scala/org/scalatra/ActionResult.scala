f94d518071c9a829ff61f7c53540d2ecb91b12dd
Naoki Takezoe <takezoe@gmail.com>
2018-09-24 17:31:18 +0900

    Render body and headers

--- core/src/main/scala/org/scalatra/ActionResult.scala
+++ core/src/main/scala/org/scalatra/ActionResult.scala
@@ -1,16 +1,30 @@
 package org.scalatra
 
 import cats.effect.IO
-import org.http4s.{Response, Status}
+import org.http4s._
 
-case class ActionResult(status: Int){
-  def toResponse(): Response[IO] = Response[IO](status = Status(status))
+case class ActionResult(
+  status: Int,
+  body: EntityBody[IO],
+  headers: Map[String, String]
+){
+  def toResponse(): Response[IO] = Response[IO](
+    status  = Status(status),
+    body    = body,
+    headers = Headers(headers.toList.map { case (name, value) => Header(name, value) })
+  )
 }
 
 trait ActionResultType[T]{
   def toActionResult(result: T): ActionResult
 }
 
-object StringActionResultType extends ActionResultType[String]{
-  def toActionResult(result: String): ActionResult = ActionResult(200)
+object StringActionResultType extends ActionResultType[String] {
+  def toActionResult(result: String): ActionResult = {
+    ActionResult(
+      status = 200,
+      body = fs2.Stream(result.getBytes("UTF-8"): _*),
+      headers = Map("Content-Type" -> "text/plain; charset=UTF-8")
+    )
+  }
 }
