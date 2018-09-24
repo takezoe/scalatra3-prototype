edcfeb6e6cab4689468cfef5fc488a7659946272
Naoki Takezoe <takezoe@gmail.com>
2018-09-24 23:39:52 +0900

    Update sample application

--- core/src/main/scala/org/scalatra/HelloController.scala
+++ core/src/main/scala/org/scalatra/HelloController.scala
@@ -2,13 +2,12 @@ package org.scalatra
 
 class HelloController extends ScalatraBase {
   get("/hello/:name"){
-    println("Hello World!")
     s"Hello ${params("name")}!"
   }
 
   get("/test/*"){
-    println(multiParams("splat"))
-    Ok("Hello!!")
+    val paths: Seq[String] = multiParams("splat")
+    Ok(paths.mkString(", "))
   }
 
   post("/test"){
